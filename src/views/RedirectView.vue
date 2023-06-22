8d1f0a876e09f9189941fae135b9b93c25480ba3
KidneyBeanRice <dydgml4616@gmail.com>
2023-06-22 21:27:47 +0900

    asdasd

--- src/views/RedirectView.vue
+++ src/views/RedirectView.vue
@@ -375,7 +375,7 @@ export default {
                 this.$axios.put("/api/members", { nickname: this.nickname, email: this.email, number: this.number }, { headers: {Authorization: Oauthtoken}})
                 .then(res => {
                     alert("회원가입이 완료되었습니다.")
-                    this.$router.go(-1);
+                    this.$router.push({ path: '/login'});
                     console.log(res)
                 })
                 .catch(error => {
